363c6a5471a03fee0889c5f82c15bf7a89437c00
iphayao <phayaob@gmail.com>
2025-08-05 23:45:09 +0700

    update title page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-[#283C8F] flex flex-col items-center justify-center p-4">
       <Head>
-        <title>Coming Soon - Our Website</title>
+        <title>Coming Soon - SoClean Tissue</title>
         <meta name="description" content="Our website is coming soon!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -21,7 +21,7 @@ export default function Home() {
           className="mb-8 rounded-lg"
         />
 
-        <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold text-white mb-6 animate-pulse">
+        <h1 className="text-3xl md:text-6xl lg:text-7xl font-extrabold text-white mb-6 animate-pulse">
           Coming Soon!
         </h1>
 
